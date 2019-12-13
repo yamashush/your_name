c6702d21516dbcc2454e5cc2f9aace36b658b801
yamashush <38120991+yamashush@users.noreply.github.com>
2019-12-13 15:33:42 +0900

    :construction: Add showing profile

--- app/app.rb
+++ app/app.rb
@@ -5,7 +5,7 @@ get '/' do
   'ok'
 end
 
-post('/') do
+post '/' do
   begin
     # slack slash commands payload
     # @see https://api.slack.com/slash-commands
@@ -18,8 +18,48 @@ post('/') do
       token: ENV['SLACK_BOT_TOKEN']
     )
     res = slack_client.users_info(user: profile_slack_user_name)
-    profile_slack_user_email = res['user']['profile']['email'] 
+    profile_slack_user_email = res['user']['profile']['email']
 
+    profile_name = ""
+    profile_photo_url = "https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png"  #dummy
+    profile_emp_no = ""
+    profile_group = ""
+    profile_position = ""
+
+    block = [
+      type: "section",
+      accessory: {
+        type: "image",
+        image_url: profile_photo_url,
+        alt_text: "photo"
+      },
+      fields: [
+				{
+          type: "mrkdwn",
+          text: "*名前*\n#{profile_name}"
+        },
+        {
+          type: "mrkdwn",
+          text: "*社員番号*\n#{profile_emp_no}"
+        },
+        {
+          type: "mrkdwn",
+          text: "*グループ*\n#{profile_group}"
+        },
+        {
+          type: "mrkdwn",
+          text: "*役職*\n#{profile_position}"
+        }
+      ]
+    ]
+
+    slack_client.chat_postEphemeral(
+      channel: requset_slack_channel_id,
+      user: request_slack_user_id,
+      blocks: block
+    )
+    
+    return # slack response
   rescue => error
     'うまく探せなかった、ごめん :bow:'
   end
