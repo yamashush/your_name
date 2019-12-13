9862e8ae708d3ff2c9fc48c5b70b13eba337150f
yamashush <38120991+yamashush@users.noreply.github.com>
2019-12-13 14:33:17 +0900

    :construction: Add receiving slash command

--- app/app.rb
+++ app/app.rb
@@ -3,3 +3,17 @@ require 'sinatra'
 get '/' do
   'ok'
 end
+
+post('/') do
+  begin
+    # slack slash commands payload
+    # @see https://api.slack.com/slash-commands
+    req = URI.decode_www_form(request.body.read)
+    slack_user_id_request = req.assoc('user_id').last
+    slack_channel_id_request = req.assoc('channel_id').last
+    slack_user_name_profile = req.assoc('text').last
+
+  rescue => error
+    'うまく探せなかった、ごめん :bow:'
+  end
+end
