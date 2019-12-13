4fdf4ab15124e0538662f754a760731053834967
yamashush <38120991+yamashush@users.noreply.github.com>
2019-12-13 15:01:28 +0900

    :construction: Add getting slack user email

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'slack-ruby-client'
 
 get '/' do
   'ok'
@@ -9,9 +10,15 @@ post('/') do
     # slack slash commands payload
     # @see https://api.slack.com/slash-commands
     req = URI.decode_www_form(request.body.read)
-    slack_user_id_request = req.assoc('user_id').last
-    slack_channel_id_request = req.assoc('channel_id').last
-    slack_user_name_profile = req.assoc('text').last
+    request_slack_user_id = req.assoc('user_id').last
+    requset_slack_channel_id = req.assoc('channel_id').last
+    profile_slack_user_name = req.assoc('text').last
+
+    slack_client = Slack::Web::Client.new(
+      token: ENV['SLACK_BOT_TOKEN']
+    )
+    res = slack_client.users_info(user: profile_slack_user_name)
+    profile_slack_user_email = res['user']['profile']['email'] 
 
   rescue => error
     'うまく探せなかった、ごめん :bow:'
